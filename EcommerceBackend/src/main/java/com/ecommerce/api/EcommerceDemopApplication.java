81cca961a5f1bd189410d2bdb0953447c9448221
MartinpcDev <martinchrispc@hotmail.com>
2025-05-12 16:12:18 -0500

    Inicio del Backend - proyecto vacio

---
+++ EcommerceBackend/src/main/java/com/ecommerce/api/EcommerceDemopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDemopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDemopApplication.class, args);
+	}
+
+}
